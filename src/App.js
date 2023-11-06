c3b766ee7c1cfb60abfc8d2831f20cdce283a760
sulaimanshuja <sulaimanshuja@gmail.com>
2023-11-06 15:00:58 +0500

    Updated Version

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h3>
+          Waste Management System
+        </h3>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn more
         </a>
       </header>
     </div>
